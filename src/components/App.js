23d3cee35025f98b68818a6dd2ec7869038858d5
DFeliciano3 <91075867+DFeliciano3@users.noreply.github.com>
2022-09-16 12:03:38 -0400

    create react app

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App (){
+    return (
+       <div>
+        <h1>My Project</h1>
+            <p>Lets Begin!</p>
+       </div> 
+    )
+}
+
+export default App;
